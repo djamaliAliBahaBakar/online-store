8821d66d0afb2e989d7faa80cc6208540991d434
Djamali Ali bakar <dalibakar@gmail.com>
2024-10-18 22:23:46 +0200

    add Loader

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,5 +1,5 @@
-import {useState} from "react";
-
+import {useState} from 'react'
+;
 export default function Product(props) {
     const [count, setCount] = useState(0);
 
@@ -14,11 +14,10 @@ export default function Product(props) {
         }
     }
 
-    if (!details){
-        return null;
-    }
+    console.log(details);
 
     return <div className="product">
+        <img width="50" alt="" src={details.image}/>
         <div className="product-info">
             <h2>{details.name}</h2>
             <p>{details.description}</p>
--- src/StoreFront.jsx
+++ src/StoreFront.jsx
@@ -1,55 +1,28 @@
-import {useState} from "react";
-import ProductsList from "./ProductsList";
-import AddProductForm from "./AddProductForm";
+import {useEffect, useState} from "react";
+import Product from "./Product";
+import Loader from './Loader'
 
 export default function StoreFront() {
     const [products, setProducts] = useState([]);
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
-    const [validation, setValidation] = useState("");
+    const [isLoading, setIsLoading] = useState(false)
 
-    function handleFormSubmit(event) {
-        event.preventDefault();
-
-        if (!name) {
-            setValidation("Please enter a name");
-            return ;
-        }
-        if (!description){
-            setValidation("Please enter a description");
-            return ;
-        }
-        setProducts([...products, {
-            id: products.length + 1,
-            name: name,
-            description: description
-        }]);
-        setName("");
-        setDescription("");
-        setValidation("");
-    }
-
-    function handleDeleteClick(id) {
-        setProducts(products.filter(product => product.id !== id));
-    }
-
-    function handleNameChanged(e) {
-        setName(e.target.value)
-    }
-
-    function handleDecriptionChanged(e) {
-        setDescription(e.target.value)
-    }
-
-    function handleValidationChanged(e) {
-        setValidation(e.target.value)
-    }
-
-    return (
-        <>
-        <AddProductForm onFormSubmit={handleFormSubmit} name={name} description={description} validation={validation} onNameChanged={handleNameChanged}  onDescriptionChanged={handleDecriptionChanged}/>
-        <ProductsList products={products} onDeleteClicked={handleDeleteClick}/>
-        
-    </>
-    )
+    useEffect(()=>{
+        setIsLoading(true)
+        fetch("https://react-tutorial-demo.firebaseio.com/products.json").then(response=>response.json()).then(data=>{
+            console.log(data);
+            if (data) {
+                setProducts(data)
+            }
+    }).catch(error=>{
+        console.log(error)
+    }).finally(()=>{
+        setIsLoading(false)
+    })
+    }, [])
+    return <div className="store-front">
+        {isLoading && <Loader />}
+        {products && products.map(product=>{
+            return <Product key={product.id} details={product}/>
+        })}
+    </div>;
 }
